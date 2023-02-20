14425b0ac5268d0f1a0337d2429d225b8b3212ef
hjcod <hjjeon5435@gmail.com>
2023-02-20 20:49:16 +0900

    asdasd

--- src/routes/orderRouter.ts
+++ src/routes/orderRouter.ts
@@ -8,7 +8,6 @@ routes.post('/buy', loginRequired, orderController.addBuyOrder)
 routes.post('/addbuybid', loginRequired, orderController.addBuyBid)
 routes.post('/sell', loginRequired, orderController.addSellOrder)
 routes.post('/addsellbid', loginRequired, orderController.addSellBid)
-
 module.exports = {
     routes
 }
