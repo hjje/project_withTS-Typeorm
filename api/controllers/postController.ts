fafd738b6d316e193a6f8d13982aa96da7c8c8f5
정재헌 <jaeheonjason@gmail.com>
2023-02-27 17:16:35 +0900

    Edit: controller

--- api/controllers/postController.ts
+++ api/controllers/postController.ts
@@ -2,18 +2,13 @@ import { Request, Response } from 'express';
 import * as postService from '../services/postService';
 import { catchAsync } from '../utils/error';
 
-// 'trending', 'newest'
-interface Filter {
-
-}
-
 const getPostByFilter = catchAsync(async(req: Request, res: Response) => {
-    // array?
+    
     const filterBy = req.query.filterBy;
     if(!filterBy) throw new Error('keyErr')
 
     const posts = await postService.getPostByFilter(filterBy);
-    console.log('controller',posts)
+
     return res.status(200).json({posts})
 })
 
