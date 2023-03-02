a929806cde8b3a01744ae273c7355453e21b73f6
정재헌 <jaeheonjason@gmail.com>
2023-03-02 14:53:09 +0900

    ADD: naver social login

--- api/controllers/userController.ts
+++ api/controllers/userController.ts
@@ -2,13 +2,24 @@ import { Request, Response } from 'express';
 import * as userService from '../services/userService'
 import { catchAsync } from '../utils/error';
 
-const kakaoLogin = catchAsync(async(req: Request, res: Response): Promise<void> => {
-    const authCode = req.query.code as string;
+const socialLogin = catchAsync(async(req: Request, res: Response): Promise<void> => {
+    const kakaoCode = req.query.code as string;
+    const naverToken = req.headers.authorization as string;
 
-    if(!authCode) throw new Error('missing Authcode');
-
-    const accessToken = await userService.kakaoLogin(authCode)
-    res.status(200).json({accessToken})
+    // kakao & naver login
+    if(!kakaoCode && !naverToken) {
+        throw new Error('missing Authcode')
+    } 
+    // kakao login
+    else if (kakaoCode && !naverToken){
+        const accessToken = await userService.kakaoLogin(kakaoCode)
+        res.status(200).json({accessToken})
+    } 
+    // naver login
+    else if (!kakaoCode && naverToken){
+        const accessToken = await userService.naverLogin(naverToken)
+        res.status(200).json({accessToken}) 
+    }
 })
 
 const getUserInfo = async(req: Request, res: Response) => {
@@ -19,6 +30,6 @@ const getUserInfo = async(req: Request, res: Response) => {
 }
 
 export { 
-    kakaoLogin,
+    socialLogin,
     getUserInfo
  }
